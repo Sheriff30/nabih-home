7345df58c488c1b421ebca5eac1fe73217c408bf
sherif <sherifdevv@gmail.com>
2025-07-22 15:10:26 +0300

    header update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { AfterViewInit } from '@angular/core';
 import { Component, signal } from '@angular/core';
 
 @Component({
@@ -6,6 +7,47 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements AfterViewInit {
+  ngAfterViewInit() {
+    const updateHeader = () => {
+      const header = document.getElementById('main-header');
+      if (header) {
+        if (window.innerWidth > 1024) {
+          header.classList.remove('sticky', 'top-0');
+          if (window.scrollY > 0) {
+            header.classList.add(
+              'bg-white/80',
+              'shadow-md',
+              'backdrop-blur-sm',
+              'z-50'
+            );
+          } else {
+            header.classList.remove(
+              'bg-white/80',
+              'shadow-md',
+              'backdrop-blur-sm',
+              'z-50'
+            );
+          }
+        } else {
+          header.classList.remove(
+            'bg-white/80',
+            'shadow-md',
+            'backdrop-blur-sm',
+            'z-50'
+          );
+          header.classList.add(
+            'sticky',
+            'top-0',
+            'backdrop-blur-sm',
+            'bg-white/80'
+          );
+        }
+      }
+    };
+    window.addEventListener('scroll', updateHeader);
+    window.addEventListener('resize', updateHeader);
+    updateHeader();
+  }
   protected readonly title = signal('nabih-home');
 }
