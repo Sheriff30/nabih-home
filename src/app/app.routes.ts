943aa774b0bf4f5cf8880697ef6c4c9db46451b0
sherif <sherifdevv@gmail.com>
2025-08-03 18:17:25 +0300

    support and privacy page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Support } from './pages/support/support';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'support', component: Support },
+  { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: '**', component: PageNotFound },
+];
---
+++ src/app/pages/support/support.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  standalone: true,
+  imports: [],
+  templateUrl: './support.html',
+  styleUrl: './support.css',
+})
+export class Support {
+  protected readonly title = 'Support - Nabih';
+}
